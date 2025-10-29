ce802741895cbcd7f132f35755fb711e78665e56
YonadavAz <104059408+YonadavAz@users.noreply.github.com>
2025-10-29 13:39:40 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,7 +1,7 @@
 def find_max_number(num1, num2, num3):
-    if num1 > num2 or num1 >num3:
+    if num1 > num2 and num1 >num3:
         return num1  # Replace 'pass' with code
-    elif num2 > num1 or num2 >num3:
+    elif num2 > num1 and num2 >num3:
         return num2
-    elif num3 > num1 or num3 > num2:
+    elif num3 > num1 and num3 > num2:
         return num3
